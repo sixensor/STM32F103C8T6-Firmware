9e4da59353947ff5a20bd26f33a05aa0489e9880
Chamith <udayangaac@gmail.com>
2020-03-26 21:43:37 +0530

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/STM32F103C8T6_Firmware.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STM32F103C8T6_Firmware.dir/main.c.o"
+  "STM32F103C8T6_Firmware"
+  "STM32F103C8T6_Firmware.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/STM32F103C8T6_Firmware.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/STM32F103C8T6_Firmware.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for STM32F103C8T6_Firmware.
+# This may be replaced when dependencies are built.
